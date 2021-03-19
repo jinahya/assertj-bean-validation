3d412bd2f2c11897a33f085d96a07f8f5273a695
Jin Kwon <onacit@wemakeprice.com>
2021-03-19 18:16:20 +0900

    Add more testcases

--- src/test/java/com/github/jinahya/assertj/validation/UserBeanValidationAssertTest.java
+++ src/test/java/com/github/jinahya/assertj/validation/UserBeanValidationAssertTest.java
@@ -1,21 +1,36 @@
 package com.github.jinahya.assertj.validation;
 
-import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.github.jinahya.assertj.validation.BeanValidationAssertions.assertBean;
+import static java.util.concurrent.ThreadLocalRandom.current;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserBeanValidationAssertTest {
 
     static Stream<User> validInstances() {
-        return IntStream.rangeClosed(0, ThreadLocalRandom.current().nextInt(128))
+        return IntStream.rangeClosed(0, current().nextInt(128))
                 .mapToObj(i -> User.newValidInstance());
+    }
+
+    static Stream<User> invalidInstances() {
+        return IntStream.rangeClosed(0, current().nextInt(128))
+                .mapToObj(i -> User.newInvalidInstance());
     }
 
     @MethodSource({"validInstances"})
     @ParameterizedTest
-    void isValid__() {
-        assertBean(User.newValidInstance()).isValid();
+    void isValid_Succeed_(final User user) {
+        assertBean(user).isValid();
+    }
+
+    @MethodSource({"invalidInstances"})
+    @ParameterizedTest
+    void isValid_Fail_(final User user) {
+        assertThrows(AssertionError.class, () -> assertBean(user).isValid());
     }
 }
