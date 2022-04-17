30823943ecddfb1e87616b4874602dc57ba60dbd
Jin Kwon <onacit@gmail.com>
2022-04-17 15:15:37 +0900

    Update javadoc

--- assertj-bean-validation-javax/src/main/java/com/github/jinahya/assertj/validation/BeanAssert.java
+++ assertj-bean-validation-javax/src/main/java/com/github/jinahya/assertj/validation/BeanAssert.java
@@ -25,7 +25,7 @@ import javax.validation.Validator;
 import java.util.function.Consumer;
 
 /**
- * An interface for verifying beans.
+ * An interface for verifying bean objects.
  *
  * @param <SELF>   self type parameter
  * @param <ACTUAL> actual type parameter
@@ -101,7 +101,7 @@ public interface BeanAssert<SELF extends BeanAssert<SELF, ACTUAL>, ACTUAL>
      * @return this assertion object.
      * @throws AssertionError when the {@code actual} is {@code null} or its current value of {@code propertyName} is
      *                        not valid.
-     * @apiNote Note that the {@link javax.validation.Valid @Valid} is not honored by the
+     * @apiNote Note that the {@link javax.validation.Valid @Valid}, as specified, is not honored by the
      * {@link Validator#validateProperty(Object, String, Class[])} method on which this method relies.
      * @implNote This method invokes {@link #hasValidProperty(String, Consumer)} method with {@code propertyName} and a
      * consumer does nothing.
--- assertj-bean-validation-javax/src/main/java/com/github/jinahya/assertj/validation/PropertyAssert.java
+++ assertj-bean-validation-javax/src/main/java/com/github/jinahya/assertj/validation/PropertyAssert.java
@@ -40,12 +40,11 @@ public interface PropertyAssert<SELF extends PropertyAssert<SELF, ACTUAL>, ACTUA
      * accepting constraint violations, if any populated, to specified consumer.
      *
      * @param beanType     the bean type; not {@code null}.
-     * @param propertyName the name of the property to validate; not {@code null}.
+     * @param propertyName the name of the property; not {@code null}.
      * @param consumer     the consumer accepts constraint violations; not {@code null}.
      * @param <T>          type of the object to validate
      * @return this assertion object.
-     * @throws AssertionError when the {@code actual} is {@code null} or is not valid for
-     *                        {@code beanType#propertyName}.
+     * @throws AssertionError when the {@code actual} is {@code null} or is not valid for {@code beanType#propertyName}.
      * @see #isValidFor(Class, String)
      */
     <T> SELF isValidFor(Class<T> beanType, String propertyName, Consumer<? super ConstraintViolation<T>> consumer);
@@ -54,11 +53,10 @@ public interface PropertyAssert<SELF extends PropertyAssert<SELF, ACTUAL>, ACTUA
      * Verifies that the {@code actual} value is valid for the property of specified name of specified bean type.
      *
      * @param beanType     the bean type; not {@code null}.
-     * @param propertyName the name of the property to validate; not {@code null}.
+     * @param propertyName the name of the property; not {@code null}.
      * @param <T>          type of the object to validate
      * @return this assertion object.
-     * @throws AssertionError when the {@code actual} is {@code null} or is not valid for
-     *                        {@code beanType#propertyName}.
+     * @throws AssertionError when the {@code actual} is {@code null} or is not valid for {@code beanType#propertyName}.
      * @apiNote Note that the {@link javax.validation.Valid @Valid} is not honored by the
      * {@link Validator#validateValue(Class, String, Object, Class[])} method on which this method relies.
      * @implNote This method invokes {@link #isValidFor(Class, String, Consumer)} method with {@code beanType},
