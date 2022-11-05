667e087a9c84a825533cb50c87d521d3c5bbb4db
Jin Kwon <onacit@gmail.com>
2022-11-06 03:09:16 +0900

    Update javdoc

--- src/main/java/com/github/jinahya/assertj/validation/PropertyAssert.java
+++ src/main/java/com/github/jinahya/assertj/validation/PropertyAssert.java
@@ -36,8 +36,9 @@ public interface PropertyAssert<SELF extends PropertyAssert<SELF, ACTUAL>, ACTUA
     /**
      * Verifies that the {@code actual} value is valid for the property of specified name of specified bean type.
      * <p>
-     * {@snippet lang = "java" id = "example":
+     * {@snippet lang="java" id="example":
      * class User {
+     *     @NotBlank String name;
      *     @Max(0x7F) @PositiveOrZero int age;
      * }
      *
@@ -45,18 +46,22 @@ public interface PropertyAssert<SELF extends PropertyAssert<SELF, ACTUAL>, ACTUA
      *     @Test void test() {
      *         // @highlight region substring="fail" type=highlighted
      *         // @link region substring="assertThatProperty" target="ValidationAssertions#assertThatProperty(Object)"
-     *         assertThatProperty( -1).isValidFor(User.class, "age"); // should fail // @highlight regex="\-?\d+" type=highlighted
-     *         assertThatProperty(300).isValidFor(User.class, "age"); // should fail // @highlight regex="\-?\d+" type=highlighted
-     *         assertThatProperty(  0).isValidFor(User.class, "age"); // should pass
-     *         assertThatProperty( 28).isValidFor(User.class, "age"); // should pass
+     *         assertThatProperty("Jane").isValidFor(User.class, "name"); // should pass
+     *         assertThatProperty(  null).isValidFor(User.class, "name"); // should fail // @highlight regex="\-?(null|name)" type=highlighted
+     *         assertThatProperty(    "").isValidFor(User.class, "name"); // should fail // @highlight regex='(\"\"|name)' type=highlighted
+     *         assertThatProperty(   " ").isValidFor(User.class, "name"); // should fail // @highlight regex='(\"\s\"|name)' type=highlighted
+     *         assertThatProperty(     0).isValidFor(User.class,  "age"); // should pass
+     *         assertThatProperty(    28).isValidFor(User.class,  "age"); // should pass
+     *         assertThatProperty(    -1).isValidFor(User.class,  "age"); // should fail // @highlight regex="\-?(\d+|age)" type=highlighted
+     *         assertThatProperty(   300).isValidFor(User.class,  "age"); // should fail // @highlight regex="\-?(\d+|age)" type=highlighted
      *         // @end
      *         // @end
      *     }
      * }
-     *}
+     * }
      *
      * @param beanType     the bean type; not {@code null}.
-     * @param propertyName the name of the property; not {@code null}.
+     * @param propertyName the name of the property; must not be {@code null}.
      * @param <T>          type of the object to validate
      * @return this assertion object.
      * @throws AssertionError when the {@code actual} is {@code null} or is not valid for
